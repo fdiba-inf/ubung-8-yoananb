0aad09c7993e43d0ccb83d1b9e3646ed74af80eb
yoananb <yoana_blagova@abv.bg>
2020-11-23 19:20:42 +0000

    Ubung 8

--- src/exercise8/Point.java
+++ src/exercise8/Point.java
@@ -1,40 +1,52 @@
 package exercise8;
 
 public class Point {
-    // Define attributes
+    public double x;
+    public double y;
 
     public Point() {
-        // Initialize attributes to 0
+        this.x=0;
+        this.y=0;
     }
 
     public Point(double x, double y) {
-        // Initialize attributes by coping x and y parameters
+        this.x=x;
+        this.y=y;
     }
 
     public Point(Point otherPoint) {
-        // Initialize attributes by coping attributes of otherPoint
+       this.x=otherPoint.x;
+       this.y=otherPoint.y;
     }
 
     public void initialize() {
-        // Change attributes through console
+      System.out.println("Input values for point");
+      System.out.println("X: ");
+      this.x = Utils.INPUT.nextDouble();
+       System.out.println("Y: ");
+       this.y = Utils.INPUT.nextDouble();
+
     }
 
     public void translate(double xDelta, double yDelta) {
-        // Change attributes by adding xDelta and yDelta
+       this.x+=xDelta;
+       this.y+=yDelta;
     }
 
     public Point createNewTranslatedPoint(double xDelta, double yDelta) {
-        // Create new point which is translated relative to this point
-        return null;
+       return new Point(this.x + xDelta, this.y + yDelta);
+        
     }
 
     public boolean equals(Point otherPoint) {
-        // Compare attributes of this point to attributes of otherPoint for equality
-        return false;
+    if(Utils.equals(this.x, otherPoint.x) && Utils.equals(this.y,otherPoint.y)){
+            return true;
+    }
+            return false;
     }
 
     public String toString() {
-        // Represent attributes as string
-        return null;
+      return String.format("(%.1f,%.1f)",this.x,this.y);
+        
     }
 }
--- src/exercise8/PointDemo.java
+++ src/exercise8/PointDemo.java
@@ -1,3 +1,4 @@
+  
 package exercise8;
 
 public class PointDemo {
